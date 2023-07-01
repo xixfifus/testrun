1203d881281c028dffe8170bad0213088db2ba5b
xixfifus <124415285+xixfifus@users.noreply.github.com>
2023-07-02 03:28:25 +0800

    Update grcconfig.sh

--- grcconfig.sh
+++ grcconfig.sh
@@ -1,61 +1,61 @@
  passwd=$1
     #passwdba624=$passwd
     config="{
-               \"inbounds\": [
-                  {
-                    \"port\": 2052, //监听端口
-                    \"protocol\": "vless",
-                    \"settings\": {
-                      \"clients\": [
-                        {
-                          \"id": "0a652466-dd56-11e9-aa37-5600024c1d6a\", //修改为自己的UUID
-                          \"email": "2052@gmail.com\"
-                        }
-                      ],
-                      \"decryption\": \"none\"
-                    },
-                    \"streamSettings\": {
-                      \"network\": \"kcp\",
-                      \"security\": \"none\",
-                      \"kcpSettings\": {
-                        \"uplinkCapacity\": 100,
-                        \"downlinkCapacity\": 100,
-                        \"congestion\": true, //启用拥塞控制
-                        \"seed\": \"60VoqhfjP79nBQyU\" //修改为自己的seed密码
+             \"inbounds\": [
+                {
+                  \"port\": 2052, //监听端口
+                  \"protocol\": "vless",
+                  \"settings\": {
+                    \"clients\": [
+                      {
+                        \"id": "0a652466-dd56-11e9-aa37-5600024c1d6a\", //修改为自己的UUID
+                        \"email": "2052@gmail.com\"
                       }
-                    },
-                    \"sniffing\": {
-                      \"enabled\": true,
-                      \"destOverride\": [
-                        \"http\",
-                        \"tls\"
-                      ]
-                    }
-                  }
-                ],
-                \"routing\": {
-                  \"rules\": [
-                    {
-                      \"type\": \"field\",
-                      \"protocol\": [
-                        \"bittorrent\"
-                      ],
-                      \"outboundTag\": \"blocked\"
-                    }
-                  ]
-                },
-                \"outbounds\": [
-                  {
-                    \"protocol\": \"freedom\",
-                    \"settings\": {}
+                    ],
+                    \"decryption\": \"none\"
                   },
+                  \"streamSettings\": {
+                    \"network\": \"kcp\",
+                    \"security\": \"none\",
+                    \"kcpSettings\": {
+                      \"uplinkCapacity\": 100,
+                      \"downlinkCapacity\": 100,
+                      \"congestion\": true, //启用拥塞控制
+                      \"seed\": \"60VoqhfjP79nBQyU\" //修改为自己的seed密码
+                    }
+                  },
+                  \"sniffing\": {
+                    \"enabled\": true,
+                    \"destOverride\": [
+                      \"http\",
+                      \"tls\"
+                    ]
+                  }
+                }
+              ],
+              \"routing\": {
+                \"rules\": [
                   {
-                    \"tag\": \"blocked\",
-                    \"protocol\": \"blackhole\",
-                    \"settings\": {}
+                    \"type\": \"field\",
+                    \"protocol\": [
+                      \"bittorrent\"
+                    ],
+                    \"outboundTag\": \"blocked\"
                   }
                 ]
-              }
+              },
+              \"outbounds\": [
+                {
+                  \"protocol\": \"freedom\",
+                  \"settings\": {}
+                },
+                {
+                  \"tag\": \"blocked\",
+                  \"protocol\": \"blackhole\",
+                  \"settings\": {}
+                }
+              ]
+            }"
 
 
     
