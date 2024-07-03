8b82daf68c7594ac7aedac34ae5771bf05d657d9
xixfifus <124415285+xixfifus@users.noreply.github.com>
2024-07-03 08:50:08 +0800

    Update testconfig2.sh

--- testconfig2.sh
+++ testconfig2.sh
@@ -1,81 +1,81 @@
- passwd=$1
+    passwd=$1
+    vlpasswd=$2
+    vlpath=$3
     #passwdba624=$passwd
-    config="```
-{
-  "log": {
-    "access": "/var/log/xray/access.log",
-    "error": "/var/log/xray/error.log",
-    "loglevel": "warning"
-  },
-  "inbounds": [
-    {
-      "tag": "8443",
-      "port": 8443,
-      "protocol": "shadowsocks",
-      "settings": {
-        "method": "aes-128-gcm",
-        "password": "HjanADnd9QIi5DTAlW533Q==",
-        "network": "tcp,udp"
-      }
-    },
-    {
-      "tag": "16888",
-      "listen": "127.0.0.1",
-      "port": 16888,
-      "protocol": "VLESS",
-      "settings": {
-        "clients": [
-          {
-            "id": "061fec8b-9120-4204-bcce-399aa2ef518c",
-            "alterId": 0
-          }
+        config="{
+        \"log\": {
+            \"access\": \"/var/log/xray/access.log\",
+            \"error\": \"/var/log/xray/error.log\",
+            \"loglevel\": \"warning\"
+        },
+        \"inbounds\": [
+            {
+            \"tag\": \"8443\",
+            \"port\": 8443,
+            \"protocol\": \"shadowsocks\",
+            \"settings\": {
+                \"method\": \"aes-128-gcm\",
+                \"password\": "\"$passwd\"",
+                \"network\": \"tcp,udp\"
+            }
+            },
+            {
+            \"tag\": \"16888\",
+            \"listen\": \"127.0.0.1\",
+            \"port\": 16888,
+            \"protocol\": \"VLESS\",
+            \"settings\": {
+                \"clients\": [
+                {
+                    \"id\": "\"$vlpasswd\"",
+                    \"alterId\": 0
+                }
+                ],
+                \"decryption\": \"none\"
+            },
+            \"streamSettings\": {
+                \"network\": \"ws\",
+                \"wsSettings\": {
+                \"path\": "\"$vlpath\""
+                }
+            }
+            }
         ],
-        "decryption": "none"
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-          "path": "DreamAutoApi16888"
+        \"outbounds\": [
+            {
+            \"tag\": \"16888\", 
+            \"protocol\": \"freedom\",
+            \"settings\": {}
+            }
+        ],
+        \"routing\": {
+            \"domainStrategy\": \"IPIfNonMatch\",
+            \"rules\": [
+            {
+                \"inboundTag\": [\"16888\"],
+                \"outboundTag\": \"16888\",
+                \"type\": \"field\"
+            },
+            {
+                \"inboundTag\": [\"8443\"],
+                \"outboundTag\": \"16888\",
+                \"type\": \"field\"
+            }
+        ]
+        },
+        \"dns\": {
+            \"servers\": [
+            \"https+local://1.1.1.1/dns-query\",
+            \"1.1.1.1\",
+            \"1.0.0.1\",
+            \"8.8.8.8\",
+            \"8.8.4.4\",
+            \"localhost\",
+            \"2001:4860:4860::8888\",
+            \"2001:4860:4860::8844\"
+            ]
         }
-      }
-    }
- ],
-  "outbounds": [
-    {
-      "tag": "16888", 
-      "protocol": "freedom",
-      "settings": {}
-    }
-  ],
-  "routing": {
-    "domainStrategy": "IPIfNonMatch",
-    "rules": [
-      {
-        "inboundTag": ["16888"],
-        "outboundTag": "16888",
-        "type": "field"
-      },
-      {
-        "inboundTag": ["8443"],
-        "outboundTag": "16888",
-        "type": "field"
-      }
-   ]
-  },
-  "dns": {
-    "servers": [
-      "https+local://1.1.1.1/dns-query",
-      "1.1.1.1",
-      "1.0.0.1",
-      "8.8.8.8",
-      "8.8.4.4",
-      "localhost",
-      "2001:4860:4860::8888",
-      "2001:4860:4860::8844"
-    ]
-  }
-}
-```"
+        }"
     
 echo  "$config" > /usr/local/etc/xray/config.json
 sudo systemctl restart xray
